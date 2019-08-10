208c82495e1d383218b1f2c77be83c8881572aec
Daniel Rodriguez <drod2793@gmail.com>
2019-08-10 10:43:36 -0500

    Updates required to app and index for material-ui

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = (
+  <div>
+    <CssBaseline />
+    <App />
+  </div>
+)
+
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
