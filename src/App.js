bb7bcf262402dc0aa3de0f13867b51647cd583e9
Daniel Rodriguez <drod2793@gmail.com>
2019-08-10 21:00:35 -0500

    Adds mui theme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import blueGrey from '@material-ui/core/colors/blueGrey';
+import cyan from '@material-ui/core/colors/cyan';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+
 import Button from '@material-ui/core/Button';
 
 import './App.css';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: blueGrey,
+    secondary: cyan
+  },
+});
+
 function App() {
   return (
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
+    <ThemeProvider theme={theme}>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </ThemeProvider>
   );
 }
 
