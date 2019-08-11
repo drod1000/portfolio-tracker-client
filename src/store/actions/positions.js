ac22c01bdec0091464fa7d15ef036a6d40d5f6d4
Daniel Rodriguez <drod2793@gmail.com>
2019-08-11 16:35:09 -0500

    Adds timeout to simulate api call

--- src/store/actions/positions.js
+++ src/store/actions/positions.js
@@ -6,19 +6,21 @@ const createData = (symbol, quantity, price) => {
 
 export const initPositions = () => {
   return dispatch => {
-    const positions = [
-      createData('AAPL', 10, 175.00),
-      createData('FB', 20, 200.00),
-      createData('SBUX', 25, 80.00),
-      createData('PEP', 15, 125.00),
-      createData('TSN', 30, 80.00),
-      createData('WM', 30, 125.00),
-      createData('TGT', 50, 75.00),
-      createData('ALL', 20, 100.00),
-      createData('DIS', 15, 125.00),
-      createData('NKE', 20, 100.00)
-    ];
-    dispatch(loadPositionsSuccess(positions));
+    setTimeout(() => {
+      const positions = [
+        createData('AAPL', 10, 175.00),
+        createData('FB', 20, 200.00),
+        createData('SBUX', 25, 80.00),
+        createData('PEP', 15, 125.00),
+        createData('TSN', 30, 80.00),
+        createData('WM', 30, 125.00),
+        createData('TGT', 50, 75.00),
+        createData('ALL', 20, 100.00),
+        createData('DIS', 15, 125.00),
+        createData('NKE', 20, 100.00)
+      ];
+      dispatch(loadPositionsSuccess(positions));
+    }, 3000)
   }
 }
 
