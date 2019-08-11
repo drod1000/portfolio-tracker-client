967647175a4069185ff1a3a54617f4693cd5557a
Daniel Rodriguez <drod2793@gmail.com>
2019-08-11 11:21:52 -0500

    Updates layout with paper

--- src/components/hoc/Layout/Layout.js
+++ src/components/hoc/Layout/Layout.js
@@ -1,20 +1,35 @@
 import React , { Component } from 'react';
+import PropTypes from 'prop-types';
+import Paper from '@material-ui/core/Paper';
+import { withStyles } from '@material-ui/styles';
 
 import Aux from '../Aux/Aux';
 import PortfolioTrackerAppBar from '../../Navigation/PortfolioTrackerAppBar/PortfolioTrackerAppBar';
 
+const styles = {
+  paper: {
+    padding: 24,
+    margin: 12
+  }
+}
+
 class Layout extends Component {
 
   render () {
+    const { classes } = this.props;
     return (
       <Aux>
         <PortfolioTrackerAppBar></PortfolioTrackerAppBar>
-        <main>
+        <Paper className={classes.paper}>
           {this.props.children}
-        </main>
+        </Paper>
       </Aux>
     )
   }
 }
 
-export default Layout;
+Layout.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(Layout);
