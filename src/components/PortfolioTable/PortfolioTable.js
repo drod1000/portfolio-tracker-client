fb52c1ff83c2d3842314838288f971ab8a2ed0e6
Daniel Rodriguez <drod2793@gmail.com>
2019-08-12 08:56:32 -0500

    Styles rows and cells

--- src/components/PortfolioTable/PortfolioTable.js
+++ src/components/PortfolioTable/PortfolioTable.js
@@ -4,30 +4,49 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import { withStyles } from '@material-ui/styles';
+
+const StyledTableCell = withStyles(theme => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  }
+}))(TableCell);
+
+const StyledTableRow = withStyles(theme => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.background.default,
+    },
+  },
+}))(TableRow);
 
 const PortfolioTable = (props) => {
   return (
     <Table>
       <TableHead>
-        <TableRow>
-          <TableCell>Symbol</TableCell>
-          <TableCell>Quantity</TableCell>
-          <TableCell>Price ($)</TableCell>
-          <TableCell>Market Value ($)</TableCell>
-          <TableCell>Gain ($)</TableCell>
-          <TableCell>Gain (%)</TableCell>
-        </TableRow>
+        <StyledTableRow>
+          <StyledTableCell align="right">Symbol</StyledTableCell>
+          <StyledTableCell align="right">Quantity</StyledTableCell>
+          <StyledTableCell align="right">Price ($)</StyledTableCell>
+          <StyledTableCell align="right">Market Value ($)</StyledTableCell>
+          <StyledTableCell align="right">Gain ($)</StyledTableCell>
+          <StyledTableCell align="right">Gain (%)</StyledTableCell>
+        </StyledTableRow>
       </TableHead>
       <TableBody>
         {props.positions.map(p => (
-          <TableRow key={p.symbol}>
-            <TableCell>{p.symbol}</TableCell>
-            <TableCell>{p.quantity}</TableCell>
-            <TableCell>${p.currentPrice.toFixed(2)}</TableCell>
-            <TableCell>${(p.currentPrice * p.quantity).toFixed(2)}</TableCell>
-            <TableCell>{((p.currentPrice - p.buyPrice) * p.quantity).toFixed(2)}</TableCell>
-            <TableCell>{((p.currentPrice - p.buyPrice) / p.currentPrice * 100).toFixed(2)}</TableCell>
-          </TableRow>
+          <StyledTableRow key={p.symbol}>
+            <StyledTableCell align="right">{p.symbol}</StyledTableCell>
+            <StyledTableCell align="right">{p.quantity}</StyledTableCell>
+            <StyledTableCell align="right">${p.currentPrice.toFixed(2)}</StyledTableCell>
+            <StyledTableCell align="right">${(p.currentPrice * p.quantity).toFixed(2)}</StyledTableCell>
+            <StyledTableCell align="right">{((p.currentPrice - p.buyPrice) * p.quantity).toFixed(2)}</StyledTableCell>
+            <StyledTableCell align="right">{((p.currentPrice - p.buyPrice) / p.currentPrice * 100).toFixed(2)}</StyledTableCell>
+          </StyledTableRow>
         ))}
       </TableBody>
     </Table>
