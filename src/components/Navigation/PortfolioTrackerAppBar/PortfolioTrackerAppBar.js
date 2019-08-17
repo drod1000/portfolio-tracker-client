65fd465ed8546cdf8cc4ad0a411090f4b5238efa
Daniel Rodriguez <drod2793@gmail.com>
2019-08-16 20:01:48 -0500

    Cleans up main app bar now that we have navlinks

--- src/components/Navigation/PortfolioTrackerAppBar/PortfolioTrackerAppBar.js
+++ src/components/Navigation/PortfolioTrackerAppBar/PortfolioTrackerAppBar.js
@@ -1,67 +1,23 @@
 import React from 'react';
-import { withRouter } from "react-router";
 import { AppBar } from '@material-ui/core';
-import IconButton from '@material-ui/core/IconButton';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import MenuIcon from '@material-ui/icons/Menu';
 import { makeStyles } from '@material-ui/styles';
 
 const useStyles = makeStyles({
   root: {
     flexGrow: 1,
     marginBottom: 24
-  },
-  menuButton: {
-    marginRight: 12
   }
 })
 
-const PortfolioTrackerAppBar = (props) => {
-  const [anchorEl, setAnchorEl] = React.useState(null);
+const PortfolioTrackerAppBar = () => {
   const classes = useStyles();
-
-  function handleMenuButtonClick(event) {
-    setAnchorEl(event.currentTarget);
-  }
-
-  function navigateToPortfolio() {
-    setAnchorEl(null);
-
-    if (props.history.location.pathname !== '/portfolio') {
-      props.history.push('/portfolio');
-    }
-  }
-
-  function navigateToWatchlist() {
-    setAnchorEl(null);
-
-    if (props.history.location.pathname !== '/watchlist') {
-      props.history.push('/watchlist');
-    }
-  }
-
-  function handleMenuClose() {
-    setAnchorEl(null);
-  }
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
-        <Toolbar>
-          <IconButton className={classes.menuButton} edge="start" aria-label="menu" onClick={handleMenuButtonClick}>
-            <MenuIcon />
-          </IconButton>
-          <Menu
-            anchorEl={anchorEl}
-            keepMounted
-            open={Boolean(anchorEl)}
-            onClose={handleMenuClose}>
-            <MenuItem onClick={navigateToPortfolio}>Portfolio</MenuItem>
-            <MenuItem onClick={navigateToWatchlist}>Watchlist</MenuItem>
-          </Menu>
+        <Toolbar className={classes.appBar}>
           <Typography variant="h6">
             Portfolio Tracker
           </Typography>
@@ -71,4 +27,4 @@ const PortfolioTrackerAppBar = (props) => {
   )
 }
 
-export default (withRouter(PortfolioTrackerAppBar));
+export default PortfolioTrackerAppBar;
